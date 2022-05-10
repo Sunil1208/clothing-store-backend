61ac71485f665280fe4223f3a0f793ae224e60b7
sunil-kumar <sunil@loadshare.net>
2022-05-11 00:42:27 +0530

    added dummy controlles for auth routes

---
+++ controllers/auth.controller.js
@@ -0,0 +1,6 @@
+exports.signout = (req, res) => {
+    return res.json({
+        message: "User signout success",
+        status: 200
+    })
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,8 @@
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/signout", (req, res) => {
-    res.send("user signout");
-});
+const { signout } = require("../controllers/auth.controller");
+
+router.get("/signout", signout);
 
 module.exports = router;
