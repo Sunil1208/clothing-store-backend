981a902141d6e90ffec5f1921685bee877025f07
sunil-kumar <sunil@loadshare.net>
2022-05-11 00:26:55 +0530

    using routers in express

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
+// MIDDLEWARES
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// MY ROUTES
+app.use("/api/v1", authRoutes);
+
+
+// DB CONNECTION
 mongoose.connect(process.env.DATABASE_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -14,8 +29,10 @@ mongoose.connect(process.env.DATABASE_URI, {
     console.log(`ERR CONNECTING DB ${err}`)
 });
 
+// PORT
 const port = process.env.PORT || 5000;
 
+// STARTING SERVER
 app.listen(port, () => {
     console.log(`App is running at port ${port}`);
 });
