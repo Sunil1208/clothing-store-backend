8bcbd416dff270b9f1cea59c13476c4c0a1eb8fc
sunil-kumar <sunil@loadshare.net>
2022-05-11 17:18:09 +0530

    using custom validators

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model");
+const { validationResult } = require("express-validator");
 
 const { success, error, validation } = require("../utils/responseApi.utils");
 
@@ -8,6 +9,13 @@ exports.signout = (req, res) => {
 };
 
 exports.signup = (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res
+                .status(422)
+                // .json(validation(errors.array()[0].msg))
+                .json(validation([errors.array().map((err) => err.msg)]));
+    }
     const user = new User(req.body);
     user.save((err, user) => {
         if(err){
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 
 const { signout, signup } = require("../controllers/auth.controller");
 
-router.post("/signup", signup);
+router.post("/signup", [
+    check("name", "Name should be at least 3 characters").isLength({ min: 3 }),
+    check("lastname", "Last name should be at least 3 characters").isLength({ min: 3 }),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password should be at least 5 characters").isLength({min: 5})
+], signup);
 router.get("/signout", signout);
 
 module.exports = router;
